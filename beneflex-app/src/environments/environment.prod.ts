bb5f5906f7397f036dd7ba4ed36864601246b13d
Duque1001 <anderson.duque@savios.com.co>
2026-02-12 18:57:22 -0500

    correccion

--- beneflex-app/src/environments/environment.prod.ts
+++ beneflex-app/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
   backendBaseUrl: 'https://beneflex-savios-backend-facxcadmesfcekbz.westcentralus-01.azurewebsites.net',
+  msal: {
+    clientId: 'c2118f1c-4ac4-4251-84de-10a4274ee244',
+    tenantId: '7f32fcbe-7a03-45a9-a3d0-2beb0fa0aaf7'
+  }
 };
