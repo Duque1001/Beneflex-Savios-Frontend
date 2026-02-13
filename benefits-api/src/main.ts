b70ffe55f6acccb9c2209214af3cfb374e59842a
Duque1001 <anderson.duque@savios.com.co>
2026-02-13 13:36:11 -0500

    correccion 6

--- benefits-api/src/main.ts
+++ benefits-api/src/main.ts
@@ -34,8 +34,10 @@ async function bootstrap() {
     }),
   );
 
-  const port = Number(process.env.PORT) || 3000;
-  await app.listen(port);
+  // const port = Number(process.env.PORT) || 3000;
+  // await app.listen(port);
+  const port = Number(process.env.PORT) || 8080;
+  await app.listen(port, '0.0.0.0');
 
   console.log(`Backend escuchando en puerto ${port}`);
 }
