127b69558ad794a8608eb4e931c11f532210bdd4
Duque1001 <anderson.duque@savios.com.co>
2026-02-17 09:30:22 -0500

    correccion 10

--- benefits-api/src/app.module.ts
+++ benefits-api/src/app.module.ts
@@ -21,34 +21,16 @@ export class AppModule {}*/
 
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { BenefitsModule } from './benefits/benefits.module';
-import { BenefitRequestsModule } from './benefit-requests/benefit-requests.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiController } from './api/api.controller';
 import { FunctionsClient } from './functions/functions.client';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-
-    TypeOrmModule.forRoot({
-      type: 'mssql', // o postgres según uses
-      host: process.env.DB_HOST,
-      port: 1433,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
-      options: {
-        encrypt: true,
-      },
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
-
-    BenefitsModule,
-    BenefitRequestsModule,
   ],
   controllers: [AppController, ApiController],
   providers: [AppService, FunctionsClient],
