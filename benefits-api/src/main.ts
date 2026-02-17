41eaef89b6f868b19d02fef2ff635f7e1b58542a
Duque1001 <anderson.duque@savios.com.co>
2026-02-17 12:38:30 -0500

    fix: use azure assigned port

--- benefits-api/src/main.ts
+++ benefits-api/src/main.ts
@@ -8,16 +8,13 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: [
-      'http://localhost:4200',
-      'https://calm-rock-0ddd0211e.6.azurestaticapps.net',
-    ],
+    origin: ['https://calm-rock-0ddd0211e.6.azurestaticapps.net'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT || 3000);
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
